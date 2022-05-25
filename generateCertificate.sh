0fa72a34e00a1443a53aa5923dbf9c17a81eaee3
RoGaTT <firestyl@yandex.ru>
2022-05-25 08:43:43 +0300

    update conf

--- generateCertificate.sh
+++ generateCertificate.sh
@@ -1,1 +1,7 @@
-docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --agree-tos --email firestyl@yandex.ru --domains petsocial.rogatt.ru
+if [ -z "$1" ]
+then
+  echo 'Domain is required';
+  exit 1;
+fi
+
+docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --agree-tos --email firestyl@yandex.ru --domains $1
