eaecb0e858c000cc7860ddb016319312eef64edc
RoGaTT <firestyl@yandex.ru>
2022-06-21 21:38:05 +0300

    update confs

--- generateCertificate.sh
+++ generateCertificate.sh
@@ -4,4 +4,4 @@ then
   exit 1;
 fi
 
-docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --agree-tos --email firestyl@yandex.ru --domains $1
+docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --agree-tos --email firestyl@yandex.ru -v --domains $1
